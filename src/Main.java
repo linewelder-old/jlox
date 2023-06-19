73bba3365955222c9987c69cba6aac4ea1c64ef8
linewelder <redalerm@gmail.com>
2023-06-19 23:16:50 +0200

    Implement entry point

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        final byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException {
+        final InputStreamReader input = new InputStreamReader(System.in);
+        final BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.println("> ");
+            final String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
+        throw new UnsupportedOperationException();
     }
 }
