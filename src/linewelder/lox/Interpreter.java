09d512899bc6e06319b11e32e0473baa995c1350
linewelder <redalerm@gmail.com>
2023-06-23 19:41:28 +0200

    Implement return statements

--- src/linewelder/lox/Interpreter.java
+++ src/linewelder/lox/Interpreter.java
@@ -95,7 +95,9 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitReturnStmt(Stmt.Return stmt) {
-        throw new UnsupportedOperationException();
+        Object value = null;
+        if (stmt.value != null) value = evaluate(stmt.value);
+        throw new Return(value);
     }
 
     @Override
