6456260b62d8b6e63cbde86139361e328d68f0e3
linewelder <redalerm@gmail.com>
2023-06-25 20:36:49 +0200

    Add check for shadowing local variables

--- src/linewelder/lox/Resolver.java
+++ src/linewelder/lox/Resolver.java
@@ -41,6 +41,10 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private void declare(Token name) {
         if (scopes.isEmpty()) return;
         final Map<String, Boolean> scope = scopes.peek();
+        if (scope.containsKey(name.lexeme)) {
+            Lox.error(name, "Already a variable with this name in this scope.");
+        }
+
         scope.put(name.lexeme, false);
     }
 
