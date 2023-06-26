c911cfe70055d6a46e81d47f272c93f0a9564af0
linewelder <redalerm@gmail.com>
2023-06-26 16:14:40 +0200

    Add class declarations

--- src/linewelder/lox/Parser.java
+++ src/linewelder/lox/Parser.java
@@ -26,6 +26,7 @@ class Parser {
 
     private Stmt declaration(boolean replPrompt) {
         try {
+            if (match(CLASS)) return classDeclaration();
             if (check(FUN) && peekNext().type != LEFT_PAREN) {
                 advance();
                 return function("function");
@@ -36,6 +37,19 @@ class Parser {
             synchronize();
             return null;
         }
+    }
+
+    private Stmt classDeclaration() {
+        final Token name = consume(IDENTIFIER, "Expect class name.");
+        consume(LEFT_BRACE, "Expect '{' before class body.");
+
+        final List<Stmt.Function> methods = new ArrayList<>();
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            methods.add(function("method"));
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after class body.");
+        return new Stmt.Class(name, methods);
     }
 
     private Stmt varDeclaration() {
--- src/linewelder/tools/GenerateAst.java
+++ src/linewelder/tools/GenerateAst.java
@@ -28,6 +28,7 @@ public class GenerateAst {
         defineAst(outputDir, "Stmt", Arrays.asList(
             "Break      : Token token",
             "Block      : List<Stmt> statements",
+            "Class      : Token name, List<Stmt.Function> methods",
             "Expression : Expr expression",
             "Function   : Token name, Expr.Function function",
             "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
